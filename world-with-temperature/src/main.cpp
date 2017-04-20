5cee83be24fb246f85992e10225c1c3fa7bf4cc6
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-04-20 20:58:20 +0300

    main()

--- world-with-temperature/src/main.cpp
+++ world-with-temperature/src/main.cpp
@@ -2,6 +2,25 @@
 // Created by Oleg Morozenkov on 30.03.17.
 //
 
+#include "TemperatureWorld.h"
+#include "TemperatureWorldUpdater.h"
+
+#include <memory>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    auto world = shared_ptr<TemperatureWorld>(new TemperatureWorld(10, 10, 10));
+    auto updater = TemperatureWorldUpdater(world);
+
+    const float kTickDelay = 0.5;
+    while (true) {
+        updater.update(kTickDelay);
+        usleep(kTickDelay * 1000000);
+        cout << "Tick (delta time = " << kTickDelay << "s)" << endl;
+    }
+
     return 0;
 }
