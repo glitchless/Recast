05b6cba1badbad4112a7c3d2b47b1cd7778b6d24
lionzxy <nikita@kulikof.ru>
2017-06-03 19:54:49 +0300

    Integrate Travis CI and Doxygen

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief Test file main.cpp
+ *
+ * This file create only for testing
+ */
+#include "stdio.h"
+/**
+ * @brief This is test doxygen autogen
+ **/
+int main(){
+    printf("Hello World");
+    return 0;
+}
