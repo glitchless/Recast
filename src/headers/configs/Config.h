5dd24365e243270373ecbb6ff6ca0d3206455490
Ansile <twoflouers@yandex.ru>
2017-06-14 15:27:19 +0300

    Correction of comments (aka grammar check)

--- src/headers/configs/Config.h
+++ src/headers/configs/Config.h
@@ -17,8 +17,8 @@
  * @brief Config class
  *
  * Using Boost ptree struct.
- * Config file hashing and buffering in memmory. (You can't get Config from constructor)
- * Please calling Config::instance for get Config.
+ * Config file hashing and buffering in memory. (You can't get an instance of Config from constructor)
+ * Please call Config::instance to get an instance of Config.
  *
  **/
 class Config {
