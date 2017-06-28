f641eb1d193eac3880d4dc8ffe631bf21039efa3
lionzxy <nikita@kulikof.ru>
2017-06-28 09:56:31 +0300

    [WIP] Cast spell

---
+++ src/core/io/network/listeners/CastSpell.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file CastSpell.cpp
+ * @author LionZXY
+ * @project Recast-server
+ * @date 28.06.17
+ * @email nikita@kulikof.ru
+ **/
+#include "io/network/listeners/CastSpell.h"
+
+CastSpell::CastSpell() : NetworkListener(2) {
+
+}
+
+char *CastSpell::onPacket(char *request, ICommandSender *sender) {
+    //TODO
+    return NULL;
+}
